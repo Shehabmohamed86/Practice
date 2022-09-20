26bd6f7335ac2a4612101e42185ee7ad6b984751
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-09-19 21:06:44 -0400

    practice

--- main.js
+++ main.js
@@ -80,19 +80,59 @@
 //   return Math.min(a,b);
 // }
 
-function pow(x,n){
-  let result = x;
-  for (let i = 1; i < n; i++){
-    result *=x
-  }
-  return result;
-}
-   let x = prompt ('x?','');
-   let n  = prompt('n?','');
-  if ( n < 1) {
-    alert (`Power ${n} is not supported, use a positive integer`)
-  }else{
-    alert(pow(x,n))
-  }
+// function pow(x,n){
+//   let result = x;
+//   for (let i = 1; i < n; i++){
+//     result *=x
+//   }
+//   return result;
+// }
+//    let x = prompt ('x?','');
+//    let n  = prompt('n?','');
+//   if ( n < 1) {
+//     alert (`Power ${n} is not supported, use a positive integer`)
+//   }else{
+//     alert(pow(x,n))
+//   }
   
 
+
+
+
+
+// let ask = (question,yes,no)=>{
+//   if(confirm(question)) yes()
+//   else no()
+// }
+
+
+// ask(
+//   'Do you agree?',
+//   () => alert('you agreed'),
+//   () => alert('you canceled the execution')
+// )
+
+
+
+
+// function add7(n){
+//   console.log(n+7);
+// }
+
+
+
+
+// function multiply (a , b) {
+//   return a * b ;
+// }
+
+
+// function capitalize (str){
+//   return str[0].toUpperCase() + str.slice(1)
+// }
+
+
+
+// function lastLetter(str){
+//   return str.slice(-1);
+// }
